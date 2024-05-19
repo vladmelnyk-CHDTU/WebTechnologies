e801ef2ac6a1556db8c350f6864abb16313e821b
Vlad Melnyk <t4wer.lol2@gmail.com>
2024-05-19 21:16:32 +0300

    add laba4

---
+++ laba4/Webpack/src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap'
+import '../css/base.css'
+import '../css/vendor.css'
+import '../css/main.css'
+import '../js/plugins.js'
+import '../js/modernizr.js'
+import '../js/pace.min.js'
+import '../js/main.js'
